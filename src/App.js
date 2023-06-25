980b22e8c8f109edd9134d4314dd48eefb302a99
abrar <abrarmohammad795@gmail.com>
2023-06-25 18:45:34 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Component } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Bookshelves from "./components/Bookshelves";
+import BookshelvesContext from "./context/BookshelvesContext";
+import BookDetails from "./components/BookDetails";
+import NotFound from "./components/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ContactUs from "./components/ContactUs";
+import Favorites from "./components/Favorites";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// use the below bookshelvesList for rendering read status of book items in Bookshelves Route
+
+class App extends Component {
+  state = { themeMode: false, myFav: [], showBtn: false };
+
+  onClickTheme = () => {
+    const { themeMode } = this.state;
+    this.setState({ themeMode: !themeMode });
+  };
+
+  onClickAddToFav = (data) => {
+    const { showBtn } = this.state;
+    if (data !== "") {
+      this.setState((prevState) => ({
+        myFav: [...prevState.myFav, data],
+        showBtn: !showBtn,
+      }));
+    }
+  };
+
+  onClickDeleteFav = (favList) => {
+    const { myFav, showBtn } = this.state;
+    const filterData = myFav.filter((each) => each.id !== favList.id);
+    this.setState({ myFav: [...filterData], showBtn: !showBtn });
+  };
+
+  render() {
+    const { themeMode, myFav, showBtn } = this.state;
+    return (
+      <BookshelvesContext.Provider
+        value={{
+          showBtn,
+          myFav,
+          themeMode,
+          onClickTheme: this.onClickTheme,
+          onClickAddToFav: this.onClickAddToFav,
+          onClickBtn: this.onClickBtn,
+          onClickDeleteFav: this.onClickDeleteFav,
+        }}
+      >
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <ProtectedRoute exact path="/" element={<Home />} />
+          <ProtectedRoute exact path="/shelf" element={<Bookshelves />} />
+          <ProtectedRoute exact path="/books/:id" element={<BookDetails />} />
+          <ProtectedRoute exact path="/contact" element={<ContactUs />} />
+          <ProtectedRoute exact path="/myfavorite" element={<Favorites />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Redirect to="/not-found" />
+        </Routes>
+      </BookshelvesContext.Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+import { FaGoogle, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import BookshelvesContext from "../../context/BookshelvesContext";
+
+const Footer = () => (
+  <BookshelvesContext.Consumer>
+    {(value) => {
+      const { themeMode } = value;
+
+      return (
+        <div className={`footer-section ${themeMode && "darkBg"}`}>
+          <div className="images-container">
+            <div className="row">
+              <FaGoogle />
+              <FaTwitter />
+              <FaInstagram />
+              <FaYoutube />
+            </div>
+            <Link className="links" to="/contact">
+              <p className="footer-title">Contact us</p>
+            </Link>
+          </div>
+        </div>
+      );
+    }}
+  </BookshelvesContext.Consumer>
+);
+
+export default Footer;
